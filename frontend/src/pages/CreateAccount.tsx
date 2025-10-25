701718c09b37a3cfb0af9cb8f85656507c0f00f9
Luis Gomez <luisg31209@gmail.com>
2025-10-25 14:27:34 -0600

    updated broken code

--- frontend/src/pages/CreateAccount.tsx
+++ frontend/src/pages/CreateAccount.tsx
@@ -19,7 +19,7 @@ function CreateAccount() {
     };
 
     return (
-        <>
+        <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-blue-50 flex items-center justify-center p-4">
             {step === 'form' && (
                 <BasicInfoForm onAccountCreated={handleAccountCreated} />
             )}
@@ -29,7 +29,7 @@ function CreateAccount() {
                     onComplete={handleQuestionnaireComplete}
                 />
             )}
-        </>
+        </div>
     );
 }
 
