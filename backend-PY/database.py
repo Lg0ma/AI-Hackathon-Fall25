5bac20c500cfedf8873093e9cc9b98c5a61c6bcd
Luis Gomez <luisg31209@gmail.com>
2025-10-25 10:54:35 -0600

    Resolve merge conflict: keep database.py

---
+++ backend-PY/database.py
@@ -0,0 +1,17 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+#Load environment variables
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY in environment!")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Create router
+# router = APIRouter()
